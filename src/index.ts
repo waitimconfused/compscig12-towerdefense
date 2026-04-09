cd3f5ce4feb3db36b5e9d232a3fe47a1db656e5f
= <elliott_3@loonielane.ca>
2026-04-09 12:22:10 -0400

    Added basic example to `src/index.ts`
    Uses a spritesheet (`assets/smile-spritesheet.png`) that has an animation of a smile transitioning into a  frown and back.
    
    This is to be replaced, but it shows how to use the `src/sprites.ts` module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
+import { sprite } from "./sprites.js";
+
 const canvas:HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
 const context:CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+sprite.registerData({
+	name: "example-sprite",
+	source: "./assets/smile-spritesheet.png",
+
+	crop: undefined,
+
+	animation: {
+		duration: 1000,
+		offset: 0,
+
+		frames: [
+			{ source: undefined, crop: { x:0,   y:0, w:100, h:100 } },
+			{ source: undefined, crop: { x:100, y:0, w:100, h:100 } },
+			{ source: undefined, crop: { x:200, y:0, w:100, h:100 } },
+			{ source: undefined, crop: { x:300, y:0, w:100, h:100 } }
+		]
+	}
+})
 
 /**
  * Main render loop
@@ -20,6 +41,16 @@ function render() {
 
 	// Clear the screen
 	context.clearRect(0, 0, canvas.width, canvas.height);
+
+
+	sprite.drawSprite({
+		name: "example-sprite",
+
+		position: [ 0, 0 ],
+
+		size: [ 100, 100 ]
+	}, context);
+
 
 	window.requestAnimationFrame(render);
 }
