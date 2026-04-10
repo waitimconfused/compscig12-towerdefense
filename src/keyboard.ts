d0b6c8c96146228cb663d56991e4789538304677
= <elliott_3@loonielane.ca>
2026-04-10 14:41:59 -0400

    Added better `RegExp`

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -9,26 +9,40 @@ type KeyboardListener = {
 	type: KeyboardListenerType,
 	key: string | RegExp,
 
-
 	callback: KeyboardListenerCallback
 };
 
+// Array to store a list of what keyboard buttons are being pressed
 var keyList: string[] = [];
+
+// Array to store a list of event listeners (see `KeyboardListener`)
 var eventListeners: KeyboardListener[] = [];
 
-export const keyboard = {
+const keyboard = {
 
+	/**
+	 * 
+	 * @param type		Specifies what kind of action the key is doing (down, up)
+	 * 
+	 * @param key		What key is being pressed (or RegExp for matching
+	 * 					any, or a list, or something emse)
+	 * 
+	 * @param callback	Function to call when the requested keyboard event occurs 
+	 */
 	addEventListener(type: KeyboardListenerType, key: string | RegExp, callback: KeyboardListenerCallback) {
 
 		eventListeners.push({ type, key, callback });
 
 	},
 
-	isKeyDown(key: string) {
+	isKeyDown(key: string | RegExp) {
 
 		for (let i = 0; i < keyList.length; i++) {
 
-			if (keyList[i] == key) return true;
+			let currentKey:string = keyList[i] as string;
+
+			if (key instanceof RegExp && currentKey.match(key)) return true;
+			if (typeof key == "string" && currentKey == key) return true;
 
 		}
 
@@ -38,6 +52,15 @@ export const keyboard = {
 
 
 	dispatchEvent(type: KeyboardListenerType, key: string, event: KeyboardEvent) {
+
+		let index = keyList.indexOf(key);
+
+		if (type == "down") {
+			if (index == -1) keyList.push(key);
+
+		} else if (type == "up") {
+			keyList.splice(index, 1);
+		}
 
 		for (let i = 0; i < eventListeners.length; i++) {
 
@@ -54,3 +77,36 @@ export const keyboard = {
 
 	}
 };
+
+document.addEventListener("keydown", (e) => {
+	if (e.repeat) return;
+
+	let key = (e.key.length == 1) ? e.key.toLowerCase() : e.key;
+
+	keyboard.dispatchEvent("down", key, e);
+
+});
+
+document.addEventListener("keyup", (e) => {
+	if (e.repeat) return;
+
+	let key = (e.key.length == 1) ? e.key.toLowerCase() : e.key;
+
+	keyboard.dispatchEvent("up", key, e);
+
+});
+
+
+
+
+
+
+
+
+
+
+keyboard.addEventListener("down", /w|a|s|d/, (e) => {
+	console.log(e.key);
+});
+
+keyboard.isKeyDown("ArrowLeft")
