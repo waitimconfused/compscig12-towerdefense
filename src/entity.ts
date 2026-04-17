d1a606b3c67612824ebb7a674a44acd0e8c9cce6
Brendan Duong <348831637@gapps.yrdsb.ca>
2026-04-17 17:38:03 -0400

    Entity File Update

--- src/entity.ts
+++ src/entity.ts
@@ -1,9 +1,10 @@
-import { View } from "./view";
+import { Position2D } from "./types.js";
+import { View } from "./view.js";
 
-type EntityModifiers = {
+type EntityModifers = {
     speed : number,
     damage : number,
-    shield : number
+    sheild : number
 }
 
 type EntityUpgrade = {
@@ -11,63 +12,58 @@ type EntityUpgrade = {
 }
 
 class Entity {
-    private parentView : View;
+    private parentView: View
+    
+    public position : Position2D = [ 0, 0 ];
+    private walkingTo : Position2D = [ 0, 0 ];
+    
+    private _health: number;
+    public get health() { return this._health };
+    private _maxHealth: number;
+    public get maxHealth() { return this._maxHealth };
+    
+    private _stats: { health : number, speed : number, regeneration : number };
+    public get stats() { return this._stats };
+    
+    private modifers: EntityModifers;
+    
+    private _currentUpgrade: number;
+    public get currentUpgrade() { return this._currentUpgrade };
 
-    private walkingTo : [number, number];
-    private position : [number, number];
-
-    private health : number;
-    private readonly maxHealth : number;
-
-    private readonly stats : { health : number, speed : number, regeneration : number }
-
-    private modifiers : EntityModifiers;
-
-    private currentUpgrade : number;
-    private readonly upgradeData : EntityUpgrade[];
+    private _upgradeData: EntityUpgrade[];
+    public get upgradeData() { return this._upgradeData };
 
     constructor (
-        view : View,
-        stats : { health : number, speed : number, regeneration : number},
+        view: View,
+        stats: { health: number; speed: number; regeneration: number }
     ) {
         this.parentView = view;
-        this.stats = stats;
-        this.maxHealth = stats.health;
-        this.health = stats.health;
-        this.position = [0,0];
-        this.walkingTo = [0,0];
-        this.currentUpgrade = 0;
+        this._stats = stats;
+        this._maxHealth = stats.health;
+        this._health = stats.health;
     }
 
-    public setPosition(x : number, y : number) : void{
+    public setPosition(x : number, y : number) : void {
         this.position = [x,y];
     }
 
-    public walkToPosition(x : number, y : number) : void{
+    public walkToPosition(x : number, y : number) : void {
         this.walkingTo = [x,y];
     }
 
-    public getPosition() : [number, number] {
-        return this.position;
-    }
-
     public setState(state : string) : void {
-
+        
     }
 
-    public takeDamage(amount : number) : void {
-        this.health -= amount;
+    public takeDamage(damage : number) : void {
+        
     }
 
-    public getHealth() : number {
-        return this.health;
-    }
-
-    public getClosestEntity() : Entity | null {
+    private getClosestEntity() : Entity | null {
         return null;
-    } 
+    }
 
-    public getClosestTargetableEntity() : Entity | null {
+    private getClosestTargetableEntity() : Entity | null {
         return null;
     }
 }
