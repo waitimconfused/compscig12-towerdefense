fb207824ec59350172b96dd4a89a4740673c22f4
Cassie <348589169@gapps.yrdsb.ca>
2026-04-17 14:45:44 -0400

    attempted to start the sandwich ts

--- src/defender.types/sandwich.ts
+++ src/defender.types/sandwich.ts
@@ -3,7 +3,16 @@ import { DefenderEntity } from "../defenderentity.js";
 
 class Sandwich{
     //create the unique properties for the defender "sandwich"
-    
     private hitCount : number;
     private sandwichLayerCount : number;
+
+    constructor(){
+
+    }
+
+    private checkSandwichStatus (hitCount : number): void{
+        
+    }
 }
+
+export{Sandwich};
