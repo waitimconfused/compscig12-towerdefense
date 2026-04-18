a7647b639b2841da3b8f28186eb90ece4636aa47
Cassie <348589169@gapps.yrdsb.ca>
2026-04-17 20:07:17 -0400

    imported view to defender entity

--- src/defenderentity.ts
+++ src/defenderentity.ts
@@ -1,5 +1,6 @@
 //import the basic entity class
 import { Entity } from "./entity.js";
+import { View } from "./view.js";
 
 class DefenderEntity extends Entity{
     //assign basic properties for all defenders
@@ -19,6 +20,8 @@ class DefenderEntity extends Entity{
     private knockbackStrength : number = 3;
     //the amount of points the player gets when upgrading aspects of a defender
     private defenderUpgradePoints : number;
+    //check if defender has been stunned
+    private isStunned : boolean = false;
 
     /**
      * passes through all the properties
@@ -30,8 +33,24 @@ class DefenderEntity extends Entity{
      * @param theKnockbackStrength 
      * @param theDefenderUpgradePoints 
      */
-    constructor (theDefenderLvl : number, theSpawnCooldown : number, theAttackCooldown : number, theDefenderCost : number, theDefenderSoldCost : number, theDefenderUpgradePoints : number, theDefenderUpgradeCost : number ){
-        super();
+   
+    constructor (view: View,
+        stats: { health: number; speed: number; regeneration: number },
+        waveNumber: number,
+        healthScale: number, 
+        theDefenderLvl : number, theSpawnCooldown : number, theAttackCooldown : number, 
+        theDefenderCost : number, theDefenderSoldCost : number, theDefenderUpgradePoints : number, 
+        theDefenderUpgradeCost : number ){
+
+        const SCALED_HEALTH = Math.floor(
+            stats.health * (healthScale ** (waveNumber - 1))
+        );
+        super(view, {
+            health: SCALED_HEALTH,
+            speed: stats.speed,
+            regeneration: stats.regeneration
+        });
+        
         this.defenderLvl = theDefenderLvl;
         this.spawnCooldown = theSpawnCooldown;
         this.attackCooldown = theAttackCooldown;
@@ -40,6 +59,8 @@ class DefenderEntity extends Entity{
         this.defenderSoldCost = theDefenderSoldCost;
         this.defenderUpgradeCost = theDefenderUpgradeCost;
     }
+
+    
 }
 
 export {DefenderEntity};
