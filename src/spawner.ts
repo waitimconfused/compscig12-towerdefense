0445f718a544ca569d2526aeba11d083a58fc381
jarcofee <deciduous4st@outlook.com>
2026-04-17 22:03:37 -0400

    Cluster Spawner Created

--- src/spawner.ts
+++ src/spawner.ts
@@ -2,7 +2,7 @@ import { View } from "./view.js";
 import { Ant } from "./enemy.types/ant.js";
 import { Raccoon } from "./enemy.types/raccoon.js";
 
-class Spawner {
+export class Spawner {
     private view : View;
     
     constructor(view : View) {
@@ -10,6 +10,21 @@ class Spawner {
     }
 
     public spawnAntCluster(waveNumber : number, x : number, y : number) : void {
-        const SPECIAL_CLUSTER = Math.random()
+        const SPAWN_SPECIAL_CLUSTER = Math.random();
+        let cluster : number = Math.min(waveNumber, 20);
+        let randomAnts : number = Math.floor(Math.random() * 6 + 3);
+        let count : number;
+
+        if (SPAWN_SPECIAL_CLUSTER < 0.1) {
+            count = cluster;
+        } else {
+            count = randomAnts;
+        }
+
+        for (let i = 0; i < count; i++) {
+            const ant = new Ant(this.view, waveNumber);
+
+            ant.spawn(0,0);
+        }
     }
 }
