85a96f23b0c6993009cf479f318b3a3227a12e8a
= <elliott_3@loonielane.ca>
2026-04-23 15:18:23 -0400

    Previewing all assets now works

--- src/assets/preview.ts
+++ src/assets/preview.ts
@@ -1,5 +1,49 @@
 import { SpriteRenderer } from "../sprites.js";
 
-let references = await SpriteRenderer.loadDefaults();
+const template:HTMLTemplateElement = document.getElementById("sprite") as HTMLTemplateElement;
+const output:HTMLDivElement = document.getElementById("sprites") as HTMLDivElement;
 
-console.log(references);
+var references:string[] = await SpriteRenderer.loadDefaults();
+references = references.sort();
+
+var canvases:HTMLCanvasElement[] = [];
+
+for (let i = 0; i < references.length; i ++) {
+
+	let clone:DocumentFragment = document.importNode(template.content, true);
+
+	canvases.push( clone.getElementById("result") as HTMLCanvasElement );
+
+	let name:HTMLParagraphElement = clone.getElementById("name") as HTMLParagraphElement;
+	name.innerText = references[i] as string;
+
+	output.appendChild(clone);
+
+
+
+}
+
+function tick() {
+
+	for (let i = 0; i < references.length; i ++) {
+
+		let canvas:HTMLCanvasElement = canvases[i] as HTMLCanvasElement;
+
+		let context:CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		let offscreenCanvas:OffscreenCanvas = SpriteRenderer.getSpriteAsOffscreenCanvas({
+			name: references[i] as string,
+			position: [ 0, 0 ],
+			size: [ 0, 0 ],
+		});
+
+		context.drawImage(offscreenCanvas, 0, 0);
+
+	}
+
+	window.requestAnimationFrame(tick);
+
+}
+
+tick();
