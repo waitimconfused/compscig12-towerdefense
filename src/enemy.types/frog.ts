6ec19cded241e1b3ceada6f25c77eaaeccbc7af9
jarcofee <deciduous4st@outlook.com>
2026-04-23 15:06:08 -0400

    Create frog and wasp classes

--- src/enemy.types/frog.ts
+++ src/enemy.types/frog.ts
@@ -1,7 +1,35 @@
 import { View } from "../view";
 import { Entity } from "../entity";
 import { EnemyEntity } from "../enemyEntity";
+import { DefenderEntity } from "../defenderentity";
 
 export class Frog extends EnemyEntity {
-    
+    public isLeaping : boolean;
+    public canLeap : boolean = true;
+
+    constructor (view : View, waveNumber : number) {
+        const STATS = {
+            health : 1,
+            speed : 0.8
+        }
+
+        super(view, STATS, waveNumber, 1.15);
+
+        this.setDrops({
+            coins : 123,
+            points : 1,
+            materialDropRate : {
+                'jar' : 1
+            }
+        })
+    }
+
+    /**
+     * Spawns Ant at given position
+     * @param x The x coordinate on the world map
+     * @param y The y coordinate on the world map
+     */
+    public spawn(x: number, y : number) : void {
+        this.setPosition(x,y);
+    }
 }
