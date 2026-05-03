12d8bdd313a8818edf7d8456f4984b1fcde4eb3f
= <elliott_3@loonielane.ca>
2026-05-03 09:22:59 -0400

    Added sections that show each sprite in each file, paths on the left side are now clickable

--- src/assets/preview.ts
+++ src/assets/preview.ts
@@ -1,4 +1,4 @@
-import { SpriteRenderer } from "../sprites.js";
+import { SpriteData, SpriteRenderer } from "../sprites.js";
 import pathsToSpriteData from "../../assets/sprites.json" with { type: "json" };
 
 const template:HTMLTemplateElement = document.getElementById("sprite") as HTMLTemplateElement;
@@ -6,36 +6,77 @@ const output:HTMLDivElement = document.getElementById("sprites") as HTMLDivEleme
 
 const sources:HTMLUListElement = document.getElementById("sources") as HTMLUListElement;
 
+SpriteRenderer.verbose = false;
+
 var references:string[] = await SpriteRenderer.loadDefaults();
 
 for (let i = 0; i < pathsToSpriteData.length; i ++) {
+}
+
+var canvases:{ [sprite:string]: HTMLCanvasElement } = {};
+
+for (let i = 0; i < pathsToSpriteData.length; i ++) {
+	
+	let path = pathsToSpriteData[i] as string;
+	let absolutePath = new URL( path, location.origin+"/assets/" ).pathname;
+	let spriteData:SpriteData|SpriteData[] = (await import(absolutePath, { with: { type: "json" } })).default;
+	
 	let li = document.createElement("li");
-	li.innerText = pathsToSpriteData[i] as string;
+	let link = document.createElement("a");
+	link.innerText = absolutePath;
+	link.href = `#file-${i}`;
+	
+	li.appendChild(link);
 	sources.appendChild(li);
+
+	let section = document.createElement("section");
+	let sectionTitle = document.createElement("p");
+	section.appendChild(sectionTitle);
+	sectionTitle.innerText = absolutePath;
+	sectionTitle.id = `file-${i}`;
+	
+	let div = document.createElement("div");
+	section.appendChild(div);
+
+	if (Array.isArray(spriteData) == false) spriteData = [spriteData];
+
+	for (let i = 0; i < spriteData.length; i ++) {
+		let data:SpriteData = spriteData[i] as SpriteData;
+		
+		let clone:DocumentFragment = document.importNode(template.content, true);
+		
+		canvases[data.name] = clone.getElementById("result") as HTMLCanvasElement;
+		
+		let name:HTMLParagraphElement = clone.getElementById("name") as HTMLParagraphElement;
+		name.innerText = data.name as string;
+		
+		div.appendChild(clone);
+	}
+	
+	
+	output.appendChild(section);
 }
+console.log(canvases);
 
-var canvases:HTMLCanvasElement[] = [];
-
-for (let i = 0; i < references.length; i ++) {
-
-	let clone:DocumentFragment = document.importNode(template.content, true);
-
-	canvases.push( clone.getElementById("result") as HTMLCanvasElement );
-
-	let name:HTMLParagraphElement = clone.getElementById("name") as HTMLParagraphElement;
-	name.innerText = references[i] as string;
-
-	output.appendChild(clone);
-
-
-
-}
+let previousTime = 0;
 
 function tick() {
 
-	for (let i = 0; i < references.length; i ++) {
+	let currentTime = performance.now();
+	let delta = currentTime - previousTime;
 
-		let canvas:HTMLCanvasElement = canvases[i] as HTMLCanvasElement;
+	if (delta < 1000 / 60) {
+		window.requestAnimationFrame(tick);
+		return;
+	}
+
+	let sprites:string[] = Object.keys(canvases);
+
+	for (let i = 0; i < sprites.length; i ++) {
+
+		let sprite:string = sprites[i] as string;
+
+		let canvas:HTMLCanvasElement = canvases[sprite] as HTMLCanvasElement;
 
 		let context:CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
 		context.clearRect(0, 0, canvas.width, canvas.height);
@@ -53,8 +94,26 @@ function tick() {
 
 	}
 
+	previousTime = currentTime;
 	window.requestAnimationFrame(tick);
 
 }
 
 tick();
+
+
+window.navigation.addEventListener("navigate", (event) => {
+
+	setTimeout(() => {
+
+		let element:HTMLAnchorElement|null = document.querySelector(location.hash);
+
+		if (!element) return;
+
+		element.style.animation = "highlight 1s linear";
+
+		element.addEventListener("animationend", () => {
+			element.style.animation = "";
+		});
+	}, 100);
+})
