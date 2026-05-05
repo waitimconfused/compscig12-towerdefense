ee7df1aee91d44f7d6bff75b0ae46d03125caa18
Cassie <348589169@gapps.yrdsb.ca>
2026-05-05 15:29:38 -0400

    made changes to corn

--- src/defender.types/corn.ts
+++ src/defender.types/corn.ts
@@ -2,7 +2,6 @@
 import { DefenderEntity } from "../defenderentity.js";
 import { EnemyEntity } from "../enemyEntity";
 import { View } from "../view.js";
-import { Kernal } from "./kernal.js";
 
 class Corn extends DefenderEntity{
 
@@ -30,7 +29,17 @@ class Corn extends DefenderEntity{
     /**the corn itself cannot deal damage, but the kernals it produces can. 
      * Therefore, the attackEnemy method is in the Kernal class
      */
-
+     //the corn will be the entity related to Corn that does the actual damage to the enemies
+    public override attackEnemy(target : EnemyEntity): void {
+        //get the targeted entity's x and y position and save it to local variables
+        let targettedEntityXPosition = this.walkingTo[0];
+        let targettedEntityYPosition = this.walkingTo[1];
+        //wait 5 seconds before proceeding
+        wait(5000);
+        //walk to the position given the constructers from corn, and get the enemy to take damge
+        this.walkToPosition(targettedEntityXPosition,targettedEntityYPosition);
+        target.takeDamage(this.defenderBaseStats.defenderAttackDamage)
+    }
 }
 
 export {Corn};
