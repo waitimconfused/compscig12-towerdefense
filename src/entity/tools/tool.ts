64960d3e19dbd127fdc8839cab55f2087116b994
Cassie <348589169@gapps.yrdsb.ca>
2026-05-22 15:21:13 -0400

    comment Tools

--- src/entity/tools/tool.ts
+++ src/entity/tools/tool.ts
@@ -3,18 +3,21 @@ import { Entity } from "../entity.js";
 
 export abstract class Tool extends Entity{
 	/**
-	 * create a map of the specific tool requirements for the tool to be created
-	 * map is used because order of tool requirements does not matter
+	 * Create a map of the specific tool requirements for the tool to be created
+	 * Map is used because order of tool requirements does not matter
 	 */
 	toolRequirements : Map<InventoryItemTag,number>;
 
 	/**
-	 * check to see if the player can create the tool
-	 * look at the tool's unique material requirements and see if they have enough
+	 * Check to see if the player can create the tool
+	 * Look at the tool's unique material requirements and see if they have enough
 	 * @param requirements the unique materials specifically neededto create the tool
+	 * Return a boolean - whether it's true the player can create it or not
 	 */
 	public abstract canCreateTool(requirements : Array<InventoryItemTag>) : boolean;
 	
+
+
 
 	
 
