dbbc88828d84d54911a6f3f944a492b1b2defcb6
jarcofee <deciduous4st@outlook.com>
2026-05-27 15:29:45 -0400

    revert changes

--- src/view/views/gameplay.ts
+++ src/view/views/gameplay.ts
@@ -4,7 +4,6 @@ import { Sandwich } from "../../entity/defender.types/sandwich.js";
 import Engine from "../../engine.js";
 import { ViewText } from "../elements/text.js";
 import { ViewSprite } from "../elements/sprite.js";
-import { Wave } from "../../wave.js";
 
 var hasPlayed = false;
 
@@ -32,7 +31,9 @@ gameplayView.addEventListener("show", () => {
 
 	if (hasPlayed) return;
 
-	Wave.newWave();
+	Raccoon.spawn(1, [ 0, 0 ]);
+	console.log(Sandwich.level);
+	Sandwich.spawn(1, [ window.innerWidth/2, window.innerHeight/2 ]);
 
 	hasPlayed = true;
 });
