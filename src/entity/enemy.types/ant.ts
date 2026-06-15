70b2263e9bae15fa662cec928600b0508f092558
jarcofee <deciduous4st@outlook.com>
2026-06-15 15:27:30 -0400

    Path saving

--- src/entity/enemy.types/ant.ts
+++ src/entity/enemy.types/ant.ts
@@ -45,6 +45,11 @@ export class Ant extends EnemyEntity {
 
 	public static path = "M0 50.0634C311.161 43.5073 285.567 549.586 561.679 457.548C837.79 365.511 1133.82 328.192 1160.05 573.793C1186.27 819.393 693.305 992.373 577.06 751.311C460.816 510.249 486.752 130.908 881.377 115.526C1276 100.145 1478.86 273.376 1553.5 492.5C1613.21 667.8 1797.04 726.431 1900 700.88"
 
+	private _disableBrain:boolean = false;
+	private currentPath:SVGPathElement|null = null;
+	private currentPathProgress:number = 0;
+	private currentPathMaxProgress:number = 0;
+
 	/**
 	 * Override, Ant takes more damage from AOE type attacks
 	 * @param dealtDamage Amount of damage dealt
@@ -120,37 +125,74 @@ export class Ant extends EnemyEntity {
 	 */
 	public async brain() {
 
-		await this.followPath(Ant.path);
+		if (this._disableBrain) return;
 
-		await this.wait(500);
+		await this.followPath(Ant.path, () => {
 
+			if (this.currentPath) {
+				console.log("RESETING POSITION")
+				this._targetPath = this.currentPath;
+				this._targetPathLength = this.currentPathProgress;
+				this._targetPathMaxLength = this.currentPathMaxProgress;
+				
+				this.currentPath = null;
+				this.currentPathProgress = 0;
+				this.currentPathMaxProgress = 0;
+			}
+
+			this.tester();
+		});
+
+	}
+
+	public async tester() {
 		// Get the closest DEFENDER entity
 		let closestEntity = Entity.nearestEntity(this, DefenderEntity);
 	
-		if (!closestEntity || closestEntity.stats.health <= 0) {
-			super.interruptTimers("walk");
+		if (!closestEntity) {
+			this._disableBrain = false;
 			return;
 		}
-	
+
+		let distance = Entity.getDistance(this, closestEntity);
+
+		if (distance >= 200) {
+			this._disableBrain = false;
+			return;
+		}
+
+		this._disableBrain = true;
+		this.currentPath = this._targetPath;
+		this.currentPathProgress = this._targetPathLength;
+		this.currentPathMaxProgress = this._targetPathMaxLength;
+
+		this.interruptTimers("walk");
+
 		// Walk toward defender
 		let interrupt = await this.walkToEntity(closestEntity);
+		if (interrupt) {
+			this._disableBrain = false;
+			return;
+		}
 
 		// Attacks
-		if (!interrupt) {
-			// Stores closest defender health
-			let defenderHealth = closestEntity.stats.health;
-			let attackInterrupt = await this.attackEntity(closestEntity);
-			
-			if (attackInterrupt) {
-				// Ant was stopped from attacking
+		// Stores closest defender health
+		let defenderHealth = closestEntity.stats.health;
+		let attackInterrupt = await this.attackEntity(closestEntity);
+		if (attackInterrupt) {
+			this._disableBrain = false;
+			return;
+		}
 
-			} else if (closestEntity.stats.health <= 0) {
-				// Regenerates if the closestEntity is dead
-				if (defenderHealth > 0 && closestEntity.stats.health <= 0) {
-					await StatusEffects.regenerateEntity(this, 5000, 2);
-				}
+		if (closestEntity.stats.health <= 0) {
+			// Regenerates if the closestEntity is dead
+			if (defenderHealth > 0 && closestEntity.stats.health <= 0) {
+				await StatusEffects.regenerateEntity(this, 5000, 2);
 			}
 		}
+
+		this._disableBrain = false;
+	
 	}
 	
 	/**
