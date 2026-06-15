26f87d42b06800ed5b9203830262904b73e675bf
waitimconfused <elliott_3@loonielane.ca>
2026-06-15 14:44:42 -0400

    Moved `Ant.path` to `DefenderEntity.path`

--- src/entity/enemy.ts
+++ src/entity/enemy.ts
@@ -42,6 +42,8 @@ export abstract class EnemyEntity extends Entity {
     // Enemy spawm location
     protected spawnLocation : Position2D = [0,0];
 
+	public static path = "M0 50.0634C311.161 43.5073 285.567 549.586 561.679 457.548C837.79 365.511 1133.82 328.192 1160.05 573.793C1186.27 819.393 693.305 992.373 577.06 751.311C460.816 510.249 486.752 130.908 881.377 115.526C1276 100.145 1478.86 273.376 1553.5 492.5C1613.21 667.8 1797.04 726.431 1900 700.88"
+
     // Ensures enemies WILL have stats
 	declare public stats: EnemyEntityStats;
 
