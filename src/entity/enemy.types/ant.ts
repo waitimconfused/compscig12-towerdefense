5bb84be610ae6c0e955b0cbd6c7a00fb07f69233
waitimconfused <elliott_3@loonielane.ca>
2026-06-15 17:07:13 -0400

    Fixed merge conflict

--- src/entity/enemy.types/ant.ts
+++ src/entity/enemy.types/ant.ts
@@ -43,16 +43,11 @@ export class Ant extends EnemyEntity {
 		]
 	}
 
-<<<<<<< HEAD
-	public static path = "M0 50.0634C311.161 43.5073 285.567 549.586 561.679 457.548C837.79 365.511 1133.82 328.192 1160.05 573.793C1186.27 819.393 693.305 992.373 577.06 751.311C460.816 510.249 486.752 130.908 881.377 115.526C1276 100.145 1478.86 273.376 1553.5 492.5C1613.21 667.8 1797.04 726.431 1900 700.88"
-
 	private _disableBrain:boolean = false;
 	private currentPath:SVGPathElement|null = null;
 	private currentPathProgress:number = 0;
 	private currentPathMaxProgress:number = 0;
 
-=======
->>>>>>> e669f25c51ceb2274e90f211c27936a1a20d055b
 	/**
 	 * Override, Ant takes more damage from AOE type attacks
 	 * @param dealtDamage Amount of damage dealt
@@ -128,16 +123,12 @@ export class Ant extends EnemyEntity {
 	 */
 	public async brain() {
 
-<<<<<<< HEAD
 		if (this._disableBrain) return;
-=======
-		await this.followPath(EnemyEntity.path);
->>>>>>> e669f25c51ceb2274e90f211c27936a1a20d055b
 
-		await this.followPath(Ant.path, () => {
+		await this.followPath(EnemyEntity.path, () => {
 
 			if (this.currentPath) {
-				console.log("RESETING POSITION")
+				console.log("RESETTING POSITION")
 				this._targetPath = this.currentPath;
 				this._targetPathLength = this.currentPathProgress;
 				this._targetPathMaxLength = this.currentPathMaxProgress;
