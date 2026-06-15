abfe2b15ae7af2ea98633eb1aa874514490cc91d
Cassie <348589169@gapps.yrdsb.ca>
2026-06-15 17:50:31 -0400

    there appears to be an error... that will be fixed later!

--- src/entity/enemy.types/ant.ts
+++ src/entity/enemy.types/ant.ts
@@ -43,14 +43,10 @@ export class Ant extends EnemyEntity {
 		]
 	}
 
-<<<<<<< HEAD
-	public static path = "M0 50.0634C311.161 43.5073 285.567 549.586 561.679 457.548C837.79 365.511 1133.82 328.192 1160.05 573.793C1186.27 819.393 693.305 992.373 577.06 751.311C460.816 510.249 486.752 130.908 881.377 115.526C1276 100.145 1478.86 273.376 1553.5 492.5C1613.21 667.8 1797.04 726.431 1900 700.88";
-=======
 	private _disableBrain:boolean = false;
 	private currentPath:SVGPathElement|null = null;
 	private currentPathProgress:number = 0;
 	private currentPathMaxProgress:number = 0;
->>>>>>> 2f8a10f80f793dd1d3800de08b43fb567fe0a830
 
 	/**
 	 * Override, Ant takes more damage from AOE type attacks
