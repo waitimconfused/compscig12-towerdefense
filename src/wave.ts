f4c6a1c3d73ec9c86e8a6a1cc5302b47f254c2e1
Brendan Duong <348831637@gapps.yrdsb.ca>
2026-06-15 20:03:17 -0400

    Update Frog

--- src/wave.ts
+++ src/wave.ts
@@ -1,5 +1,4 @@
 import { Cherry } from "./entity/defender.types/cherry.js";
-import { Corn } from "./entity/defender.types/corn.js";
 import { Strawberry } from "./entity/defender.types/strawberry.js";
 import { EnemyEntity } from "./entity/enemy.js";
 import { Ant } from "./entity/enemy.types/ant.js";
@@ -24,42 +23,46 @@ export class Wave extends StaticClass {
 	private static _waveDuration : number = 60000;
 	private static _timeLeft : number = 60000;
 	
+	private static _waveActive = true;
+	private static _waveInitialized = false;
+
 	/**
 	 * Game loop updates the wave time
-	 * @param deltaTime The time of each game tick
+	 * @param t The time of each game tick
 	 */
-	public static update(deltaTime : number) : void {
+	public static update(t : number) : void {
+		if (!this._waveInitialized) return;
+
+		// Updates the current time left
+		this._timeLeft -= t;
 		
-		// Store if a wave is active (any enemies are alive)
 		// Initially assume all enemies are dead
-		let waveActive = true;
+		let enemyDead = true;
 		
 		// Search map for any enemies
 		// Breaks loop and sets enemyDead to be false if an enemy is found
 		for (const entity of Entity.entities.values()) {
-			if (entity instanceof EnemyEntity) {
-				waveActive = false;
+			if (entity.entityType.startsWith("enemy")) {
+				enemyDead = false;
 				break;
 			}
 		}
 
-		// If a wave is active, do not do anything
-		if (waveActive) return;
+		// Checks if the wave is finished
+		if ((this._timeLeft <= 0 || enemyDead) && this._waveActive) {
+			this._waveActive = false;
 
-		// Updates the current time left
-		this._timeLeft -= deltaTime;
+			// Starts a new wave of enemies
+			this.newWave();
 
-		// If the time is not up, do not do anything
-		if (this._timeLeft > 0) return;
+			// Increases the wave duration by 2 seconds every time a wave ends
+			this._waveDuration += 2000;
 
-		// Starts a new wave of enemies
-		this.newWave();
+			// Updates current time left to match the wave duration
+			this._timeLeft = this._waveDuration;
 
-		// Increases the wave duration by 2 seconds every time a wave ends
-		this._waveDuration += 2000;
-
-		// Reset the wave timeout
-		this._timeLeft = this._waveDuration;
+			this._waveActive = true;
+		}
 	}
 
 	public static setWave(number:number=this._waveNumber) {
@@ -70,17 +73,17 @@ export class Wave extends StaticClass {
 		Ant.antSpawn([0,0],100);
 		Raccoon.spawn(1,[100,100],2);
 		Wasp.spawn(1,[100,100],2);
-
+		
 		// Spawns a frog every 2 waves
 		if (this._waveNumber % 2 == 0) {
 			Frog.spawn(1,[100,100],2);
-		}
-
+		}	
+		
 		// Spawns a wasp every 3 waves
 		if (this._waveNumber % 3 == 0) {
 			Wasp.spawn(1,[100,100],2);
 		}
-
+		
 		// Spawns a raccoon every 5 waves
 		if (this._waveNumber % 5 == 0) {
 			Raccoon.spawn(1,[100,100],2);
@@ -93,6 +96,8 @@ export class Wave extends StaticClass {
 	 * Spawns enemies based on wave number
 	 */
 	public static newWave() : void {	
+		this._waveInitialized = true;
+
 		// Increase the wave number
 		this._waveNumber++;
 
