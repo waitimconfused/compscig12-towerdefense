9d3d572b995730f73d3c551fae5c38e187278a1e
waitimconfused <elliott_3@loonielane.ca>
2026-06-15 22:57:56 -0400

    Fixed Kernel spelling

--- src/entity/defender.types/corn.ts
+++ src/entity/defender.types/corn.ts
@@ -108,7 +108,7 @@ export class Kernel extends Corn{
 
 	public static override showInInventory: boolean = false;
 
-	//Kernal will have its own base stats
+	//Kernel will have its own base stats
 	//This means that enemies have the opportunity to get rid of the kernel before it hits them, but it doesn't directly effect the corn itself
 	public static override baseStats: DefenderEntityStats = {
 		health: 1,
